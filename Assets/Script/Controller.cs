c07f99b92ec6c2cbda95a14ab2f5f32c6b5e8391
Haobo <venusthecreation@gmail.com>
2019-07-04 01:08:44 +0800

    创建工程
    创建工程

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private GameObject mCenterBtn;
+    private bool panelOpened = false;
+
+
+    void Awake()
+    {
+        mCenterBtn = transform.Find("CenterBtn").gameObject;
+        mCenterBtn.GetComponent<LongPressTrigger>().OnLongPressTriggered += () => {
+            panelOpened = !panelOpened;
+            Refresh();
+        };
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Refresh()
+    {
+
+    }
+}
---
+++ Assets/Script/LongPressTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LongPressTrigger : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    private static float LONG_PRESSS_TIME = 0.6f;
+    private WaitForSeconds waitForLongPress = new WaitForSeconds(0.02f);
+    private Coroutine mCounterCor;
+
+    public Action OnLongPressTriggered;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator LongPressCounter()
+    {
+        float count = 0;
+        while(count < LONG_PRESSS_TIME)
+        {
+            yield return waitForLongPress;
+            count += 0.02f;
+        }
+        Debug.Log("LongPress on " + gameObject.name + " triggered");
+        if (OnLongPressTriggered != null)
+            OnLongPressTriggered();
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        mCounterCor = StartCoroutine(LongPressCounter());
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        StopCoroutine(mCounterCor);
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        StopCoroutine(mCounterCor);
+    }
+}
